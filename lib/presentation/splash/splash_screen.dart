42b0af5aab7717facaa2fab5ca66b2536fc78773
adib.bachtiar <purboindra@gmail.com>
2024-05-22 08:08:05 +0700

    auto login func

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,12 +1,18 @@
 import 'package:assets_mobile/config/constants.dart';
+import 'package:assets_mobile/config/shared_preferences_config.dart';
 import 'package:assets_mobile/presentation/splash/provider/splash_provider.dart';
 import 'package:assets_mobile/presentation/widgets/background_image_image.dart';
+import 'package:assets_mobile/repositories/auth/provider/auth_repository_provider.dart';
 import 'package:assets_mobile/route/route_name.dart';
 import 'package:assets_mobile/route/route_provider.dart';
+import 'package:assets_mobile/utils/app_key.dart';
 import 'package:assets_mobile/utils/app_print.dart';
 import 'package:assets_mobile/utils/app_text_Style.dart';
+import 'package:assets_mobile/utils/extenstion.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../login/provider/login_provider.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
@@ -25,15 +31,35 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
 
   void delayed() {
     Future.delayed(const Duration(seconds: 1), () async {
+      final token = await SharedPreferencesHelper.getString(AppKey.token);
+      final username = await SharedPreferencesHelper.getString(AppKey.username);
+      final email = await SharedPreferencesHelper.getString(AppKey.email);
+      final appDb = await SharedPreferencesHelper.getString(AppKey.appDB);
+      final version = await SharedPreferencesHelper.getString(AppKey.version);
+
       ref.read(isDemoProvider.notifier).update((state) => AppConstants.isDemo);
-      final checkExpired = ref.read(checkExpiredProvider).value;
-      AppPrint.debugLog("CHECK EXPIRED: $checkExpired");
-      if (mounted) {
-        if (checkExpired == null || checkExpired) {
-          ref.read(routerProvider).go(RouteName.login);
+      ref.read(tokenProvider.notifier).update((state) => token);
+      ref.read(usernameProvider.notifier).update((state) => username);
+      ref.read(emailProvider.notifier).update((state) => email);
+      ref.read(versiProvider.notifier).update((state) => version);
+      ref.read(databaseProvider.notifier).update((state) => appDb);
+      ref.read(apiUrlProvider.notifier).update((state) => BaseUrl.ipAddressApi);
+
+      if (token.isNotEmpty) {
+        final checkExpired = token.checkExpiredToken;
+        AppPrint.debugLog("CHECK EXPIRED: $checkExpired");
+        if (checkExpired == true) {
+          final result = await ref.read(authRepositoryProvider).refreshToken();
+          await SharedPreferencesHelper.saveData(
+              {AppKey.token: result["access_token"]});
+          await Future.delayed(const Duration(seconds: 1));
+
+          ref.read(routerProvider).go(RouteName.main);
         } else {
           ref.read(routerProvider).go(RouteName.main);
         }
+      } else {
+        ref.read(routerProvider).go(RouteName.login);
       }
     });
   }
@@ -58,7 +84,8 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
                 ),
                 Text(
                   "ASSETS MOBILE",
-                  style: AppTextStyle.titleTextStyle.copyWith(color: Colors.white, fontSize: 48),
+                  style: AppTextStyle.titleTextStyle
+                      .copyWith(color: Colors.white, fontSize: 48),
                 ),
               ],
             ),
